eb084539262dfaf8ab915410f2e969a158b08a1e
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-10-14 22:33:44 +0000

    starting on static layout

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <header class="blog-header py-3">
+            <div class="row flex-nowrap justify-content-between align-items-center">
+              <div class="col-4 pt-1">
+                <a class="btn btn-sm btn-outline-secondary" href="#">View Reviews</a>
+              </div>
+              <div class="col-4 text-center">
+                <a class="blog-header-logo text-dark text-uppercase font-weight-bold" href="#">Film Reviews</a>
+              </div>
+              <div class="col-4 d-flex justify-content-end align-items-center">
+
+                <a class="btn btn-sm btn-outline-secondary" href="#">Add a review</a>
+              </div>
+            </div>
+          </header>
+
+          <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+            <div class="col-md-6 px-0">
+              <h1 class="display-4 font-italic">Beautiful Boy (2018)</h1>
+              <p class="lead my-3">Based on the best-selling pair of memoirs from father and son David and Nic Sheff, Beautiful Boy chronicles the heartbreaking and inspiring experience of survival, relapse, and recovery in a family coping with addiction over many years.</p>
+              <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+            </div>
+          </div>
+
+          <div class="row mb-2">
+        <div class="col-md-6">
+          <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+              <strong class="d-inline-block mb-2 text-primary">Thriller</strong>
+              <h3 class="mb-0">
+                <a class="text-dark" href="#">Bad Times at the El Royale (2018)</a>
+              </h3>
+              <div class="mb-1 text-muted">Oct 3</div>
+              <p class="card-text mb-auto">If you like Tarantino movies.. This is for you.. Mind blowing screen play, ensemble cast, cinematography, music. I'll not be surprised if it bags few Academy nominations. Keeps the edge of the dear the whole time. I didn't for one bit felt the movie was lengthy. It was needed and well paced.</p>
+              <a href="#">Continue reading</a>
+            </div>
+            <img class="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+              <strong class="d-inline-block mb-2 text-success">Action</strong>
+              <h3 class="mb-0">
+                <a class="text-dark" href="#">Venom</a>
+              </h3>
+              <div class="mb-1 text-muted">Oct 3</div>
+              <p class="card-text mb-auto">We just came out of the theatre and I can honestly say. I liked it! It was funny and it paid great homage to the Todd McFarlane bombastic Venom era of the late 90's. It truly baffles me that Sony would not take enough risks to release the film with an R rating.</p>
+              <a href="#">Continue reading</a>
+            </div>
+            <img class="card-img-right flex-auto d-none d-lg-block" data-src="js/holder.min.js/200x250?theme=thumb" alt="Card image cap">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
